0169c90d992008c095e296f36ca2c0edb06c3f8c
Vinicius Santos <viniciusantos.florencio@gmail.com>
2020-09-15 17:52:16 -0300

    Criando nossos models e dando um migrate

--- Livraria/app_livraria/models.py
+++ Livraria/app_livraria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Article(models.Model):
+    Autor = models.CharField(max_length = 50)
+    Editora = models.CharField(max_length = 50)
+    Paginas = models.IntegerField()
+    Titulo = models.CharField(max_length = 50)
+    ano = models.IntegerField()
+    Email = models.EmailField(null = True)
+
+
+    def __str__(self):
+        return self.headline
+
+
 # Create your models here.
