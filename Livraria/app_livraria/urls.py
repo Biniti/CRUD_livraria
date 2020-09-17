040be06e6d54a75a081cb7b1cdb71a7a326539e5
Vinicius Santos <viniciusantos.florencio@gmail.com>
2020-09-16 21:14:31 -0300

    Criando uma listagem de pessoas

--- Livraria/app_livraria/urls.py
+++ Livraria/app_livraria/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from .views import *
 
 urlpatterns = [
-
-
+       url(r'^livro_list/', livro_list, name= 'listar_livros'),
 ]
--- Livraria/app_livraria/views.py
+++ Livraria/app_livraria/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.shortcuts import render
-from .models import livro
+from .models import *
+from django.forms import ModelForm
 
+class LivroForm(ModelForm):
+    class Meta:
+        model = livro
+        fields = ['Autor', 'Editora', 'Paginas', 'Titulo', 'ano', 'Email']
+
+
+def livro_list(request, template_name='livro_list.html'):
+    Livro = livro.objects.all()
+    Livros = {'lista': Livro}
+    return render(request, template_name, Livros)
 
 # Create your views here.
