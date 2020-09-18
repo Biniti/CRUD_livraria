9ec0277acceb11cbbe3baff366ad4221e8e52300
Vinicius Santos <viniciusantos.florencio@gmail.com>
2020-09-17 21:55:57 -0300

    Adicionado um edit e um remove

--- Livraria/app_livraria/views.py
+++ Livraria/app_livraria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.forms import ModelForm
 
@@ -12,5 +12,39 @@ def livro_list(request, template_name='livro_list.html'):
     Livro = livro.objects.all()
     Livros = {'lista': Livro}
     return render(request, template_name, Livros)
+
+def livro_new(request, template_name ='livro_form.html'):
+    form = LivroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_livros')
+    return render(request, template_name, {'form':form})
+
+
+def livro_edit (request, pk, template_name='livro_form.html'):
+    Livro = get_object_or_404(livro, pk=pk)
+    if request.method == "POST":
+        form = LivroForm(request.POST, instance = Livro)
+        if form.is_valid():
+            Livro = form.save()
+            return redirect('listar_livros')
+    else:
+        form = LivroForm(instance = Livro)
+
+    return render(request, template_name, {'form': form})
+
+
+def livro_remove(request, pk):
+    Livro = livro.objects.get(pk=pk)
+
+    if request.method == "POST":
+
+        Livro.delete()
+
+        return redirect('listar_livros')
+
+    return render(request, 'livro_delete.html', {'Livro': Livro})
+
+
 
 # Create your views here.
